ccd5bcdb170069ae1b2c48cf335d8c710016d40e
jam06452 <jam@jam06452.uk>
2026-02-12 17:36:37 +0000

    chore: add font preload and vscode settings
    - Add preload directive for Inter font stylesheet
    - Add VS Code tasks configuration for development

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,6 +57,7 @@
     <link rel="icon" href={favicon} />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </svelte:head>
 
